486c0e0c0238fb3bdfb6bbd517f6849a097fba47
Emily Schmidtling <eyschmidtling@gmail.com>
2016-09-27 01:25:12 -0700

    still trying to address cors issue

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ function debugReq(req, res, next) {
 function allowCors(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
 
   if('OPTIONS' == req.method) {
     res.send(200);
